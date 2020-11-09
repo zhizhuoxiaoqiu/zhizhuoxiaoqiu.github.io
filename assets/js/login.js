96c2d3621a126c24f8106263fa0ce60ecb62ae03
zhizhuoxiaoqiu <zx1601288258@outlook.com>
2020-11-09 23:15:27 +0800

    update

---
+++ assets/js/login.js
@@ -0,0 +1,42 @@
+if ('addEventListener' in window) {
+    window.addEventListener('load', function () {
+        document.body.className = document.body.className.replace(/\bis-preload\b/, '');
+    });
+    document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
+}
+
+function login() {
+    var login = {
+        "username": null,
+        "password": null
+    }
+    var re = {
+        "status": 500,
+        "reason": "",
+        "body": {
+            "nickname": "nickname",
+            "Profile": "../../images/1.jpg"
+        }
+    }
+    login.username = document.getElementById("username").value;
+    login.password = document.getElementById("ppassword").value;
+    if (login.username == "") {
+        re.reason = "请输入用户名";
+    } else if (login.password == "") {
+        re.reason = "请输入密码";
+    } else if (login.username != "admin" || login.password != "admin") {
+        re.reason = "用户名或密码错误";
+    } else {
+        re.body = {
+            "nickname": "nickname",
+            "Profile": "../../images/1.jpg"
+        }
+        re.reason = "登陆成功";
+        re.status = 400;
+    }
+    alert(re.reason);
+    if (re.status == 400) {
+        
+    }
+    
+}
